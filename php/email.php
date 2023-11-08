41e3b6d40736461b50fdb35efb9204bb81522133
GabsFns <gabriel0917py@gmail.com>
2023-11-08 20:38:00 -0300

    Tela Principal completa, Tela About completa, tela de projetcs em andamento, tela de contato em andamanto, Tela principal | Tela About = Reponsivo

---
+++ php/email.php
@@ -0,0 +1,29 @@
+<?php   //Variáveis
+  $email = $_POST['email'];
+  $nome = $_POST['nome'];
+  $mensagem = $_POST['mensagem'];
+  
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado</p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "fnsprogamming123@gmail.com";
+  $assunto = "Contato pelo Site";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../contact.html'>";?>
